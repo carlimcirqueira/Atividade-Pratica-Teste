47234df3d7b77dc74cd73ff815c7dee67845485b
Carlos Carlim <carlimcarlim123@gmail.com>
2026-06-17 14:30:18 -0300

    Mudanças em server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,32 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/rota');
 
-require('./database');
-
 const app = express();
 
 app.use(express.json());
 
-// 🔥 CORS FORÇADO (SEM DEPENDER DE SORTE)
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://atividade-pratica-sistema-cadastro-autenticacao-cw32ozvsc.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
 app.use(cors({
-  origin: "https://atividade-pratica-sistema-cadastro-autenticacao-cw32ozvsc.vercel.app"
-}));
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT' , 'DELETE' , 'OPTIONS'],
+    allowedHeaders: ['Content-Type']
+})); 
 
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
