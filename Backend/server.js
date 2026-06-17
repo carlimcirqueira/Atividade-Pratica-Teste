c5492395dda8977ecb8c1d69f638392c0af66363
Taylanne <taylannecastelobrancocavalcant@gmail.com>
2026-06-17 09:57:31 -0300

    Arrumando

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 
 // para usar normal, de cima para aplicação da web
 
-const PORT = 3000;
+//const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando com sucesso na porta ${PORT}`);
 });
