f46f3cc005f6df6167b37e82b6294ce364dab424
Taylanne <taylannecastelobrancocavalcant@gmail.com>
2026-06-17 12:50:23 -0300

    Teste CORS

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,13 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
+
+app.options(/.*/, cors());
 
 app.use(routes);
 
